93825a2e0e87b79bd83e87a28470aeaadf155179
Randy Layne <randy.layne@teamtreehouse.com>
2021-08-22 21:57:13 -0400

    Add active storage and fix scroll spy

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,17 @@
 class WelcomeController < ApplicationController
-  @landing-info = OpenStruct {
-    
-  }
+  def index
+    @landing_info = OpenStruct.new(
+      tag_line: 'Software Developer',
+      profile: OpenStruct.new(
+        quote: 'Good software, like wine, takes time.',
+        quote_author: 'Joel Spolsky',
+        name: 'Randy Layne',
+        about_me: "I'm a Ruby on Rails developer with an insatiable thirst for
+                    learning more about software development, including other
+                    languages.",
+        age: 43,
+        location: 'Lynchburg, VA. USA'
+      )
+    )
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import '../bootstrap/bootstrap_files.js'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
