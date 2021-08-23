ff9bfc497b1e4cc30d69b271250e1ce07c33e97e
Randy Layne <randy.layne@teamtreehouse.com>
2021-08-23 15:09:29 -0400

    Add paritals for education, skills, and experience

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,7 +11,131 @@ class WelcomeController < ApplicationController
                     languages.",
         age: 43,
         location: 'Lynchburg, VA. USA'
-      )
+      ),
+      experience: [
+        OpenStruct.new(
+          start_date: 'Sep 2018',
+          end_date: 'Sep 2021',
+          company: 'Treehouse',
+          title: 'Web Developer III',
+          location: 'Remote',
+          description: "Worked as a Ruby on Rails developer for the main
+                        application as well as filled in as a Data Engineer when
+                        the team did not have one."
+        ),
+        OpenStruct.new(
+          start_date: 'Sep 2018',
+          end_date: 'Sep 2021',
+          company: 'Treehouse',
+          title: 'Web Developer III',
+          location: 'Remote',
+          description: "Worked as a Ruby on Rails developer for the main
+                        application as well as filled in as a Data Engineer when
+                        the team did not have one."
+        ),
+        OpenStruct.new(
+          start_date: 'Sep 2018',
+          end_date: 'Sep 2021',
+          company: 'Treehouse',
+          title: 'Web Developer III',
+          location: 'Remote',
+          description: "Worked as a Ruby on Rails developer for the main
+                        application as well as filled in as a Data Engineer when
+                        the team did not have one."
+        )
+      ],
+      education: [
+        OpenStruct.new(
+          start_date: 'Sep 2018',
+          end_date: 'Sep 2021',
+          school: 'Old Dominion University',
+          description: "In a combined Bachelor’s/Master’s Degree program where
+                        I am able to work on both degrees at once.",
+          gpa: 3.93
+        ),
+        OpenStruct.new(
+          start_date: 'Sep 2018',
+          end_date: 'Sep 2021',
+          school: 'Old Dominion University',
+          description: "In a combined Bachelor’s/Master’s Degree program where
+                        I am able to work on both degrees at once.",
+          gpa: 3.93
+        ),
+        OpenStruct.new(
+          start_date: 'Sep 2018',
+          end_date: 'Sep 2021',
+          school: 'Old Dominion University',
+          description: "In a combined Bachelor’s/Master’s Degree program where
+                        I am able to work on both degrees at once.",
+          gpa: 3.93
+        )
+      ],
+      languages: [
+        OpenStruct.new(
+          name: "Ruby on Rails",
+          rating: 4
+        ),
+        OpenStruct.new(
+          name: "Ruby on Rails",
+          rating: 4
+        ),
+        OpenStruct.new(
+          name: "Ruby on Rails",
+          rating: 4
+        ),
+        OpenStruct.new(
+          name: "Ruby on Rails",
+          rating: 4
+        ),
+        OpenStruct.new(
+          name: "Ruby on Rails",
+          rating: 4
+        )
+      ],
+      development_tools: [
+        OpenStruct.new(
+          name: "Github",
+          rating: 4
+        ),
+        OpenStruct.new(
+          name: "Github",
+          rating: 4
+        ),
+        OpenStruct.new(
+          name: "Github",
+          rating: 4
+        ),
+        OpenStruct.new(
+          name: "Github",
+          rating: 4
+        ),
+        OpenStruct.new(
+          name: "Github",
+          rating: 4
+        )
+      ],
+      productivity: [
+        OpenStruct.new(
+          name: "Jira",
+          rating: 4
+        ),
+        OpenStruct.new(
+          name: "Jira",
+          rating: 4
+        ),
+        OpenStruct.new(
+          name: "Jira",
+          rating: 4
+        ),
+        OpenStruct.new(
+          name: "Jira",
+          rating: 4
+        ),
+        OpenStruct.new(
+          name: "Jira",
+          rating: 4
+        )
+      ]
     )
   end
 end
